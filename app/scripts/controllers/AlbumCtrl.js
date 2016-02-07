54780764ad34b7d32deaa61016f18fdb091a4823
teckjon <teckjonlim@gmail.com>
2016-02-06 17:32:51 -0800

    Assignment 5 completed, Controllers

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,8 @@
+ (function () {
+     function AlbumCtrl() {}
+        this.heroTitle = "Turn the Music Up!";
+
+     angular
+         .module('blocJams')
+         .controller('AlbumCtrl', AlbumCtrl);
+ })();
